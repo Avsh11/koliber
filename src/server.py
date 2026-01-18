491fd8488ff1ccaf68c10079c62ac528adb6c1f9
Krystian <96619815+Avsh11@users.noreply.github.com>
2026-01-18 14:25:47 +0100

    Użycie wątków
    Obsługa wielu userow na raz, na ten moment wiadomosci z dwoch maszyn wyswietlaja sie na serwerze, dalej trzeba juz zaimplementowac logike zeby mogli ze soba rozmawiac.

--- src/server.py
+++ src/server.py
@@ -1,6 +1,19 @@
 import socket
 import threading
 
+def multiple_users(user, addr):
+    while True:
+        try:
+            data = user.recv(1024)
+            if not data:
+                print(f"User has disconnected")
+                break
+            message = data.decode('utf-8')
+            print(f"User message: {message}")
+        except Exception as e:
+            print(f"ERROR: {e}")
+            break
+    user.close()
 #sockets/gniazda to nic innego jak punkty koncowe komunikacji
 #internet socket i TCP czyli typ gniazda (UDP czy TCP), tworzymy obiekt
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,16 +27,6 @@ print("Waiting for user connection")
 while True:
     user, addr = server.accept()
     print(f"User with address {addr} has connected")
-
-    while True:
-        try:
-            data = user.recv(1024)
-            if not data:
-                print("User has disconnected")
-                break
-            message = data.decode('utf-8')
-            print(f"User message: {message}")
-        except:
-            break
-    user.close()
+    thread = threading.Thread(target=multiple_users, args=(user,addr))
+    thread.start()
 
