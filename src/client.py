533d2749d1f6ed13e59cf7c15e430cb90b7502bd
Krystian <96619815+Avsh11@users.noreply.github.com>
2026-01-22 22:45:31 +0100

    end. beta - place for fixes

--- src/client.py
+++ src/client.py
@@ -1,9 +1,109 @@
 import socket
+import threading
+import os
+import time
 
-#to samo co na serwerze, tworzymy gniazdo dla usera i to samo, przez internet i protokol TCP lecimy i laczymy sie do serwera o adresie tym i tym. Dla UDP by bylo SOCK_DGRAM np.
-user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-user.connect(('192.168.10.1', 55555))
+#kody ansi - to wziete z internetu, po co? zeby sterowac terminalem bez bibliotek.
+#gdzie zielony to kolor z perspektywy usera, czerwony - komunikat o odejsciu, zolty o dolaczeniu, reszta kolejno reset powrot do standardowych ustawien terminala, bold pogrubienie tekstu zeby zwiekszyc cyztelnosc, up przesuniecie o linie w gore zeby nadpisywac input
+GREEN = "\033[92m"
+RED = "\033[91m"
+YELLOW = "\033[93m"
+RESET = "\033[0m"
+BOLD = "\033[1m"
+UP = "\033[F"
+CLEAR = "\033[K"
+
+#zmienna globalna przechowujaca stan tozsamosci sesji
+my_id = "???"
+
+#funkcja dekoracyjna, rozpoznaje system operacyjny i czysci ekran/
+def clear_console():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+#wyswietlenie loga ascii: czym robione: https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type+&x=none&v=4&h=4&w=80&we=false
+ASCII_LOGO = """
+                          ,--,                                            
+       ,--.  ,----..   ,---.'|                                            
+   ,--/  /| /   /   \  |   | :      ,---,    ,---,.     ,---,.,-.----.    
+,---,': / '/   .     : :   : |   ,`--.' |  ,'  .'  \  ,'  .' |\    /  \   
+:   : '/ /.   /   ;.  \|   ' :   |   :  :,---.' .' |,---.'   |;   :    \  
+|   '   ,.   ;   /  ` ;;   ; '   :   |  '|   |  |: ||   |   .'|   | .\ :  
+'   |  / ;   |  ; \ ; |'   | |__ |   :  |:   :  :  /:   :  |-,.   : |: |  
+|   ;  ; |   :  | ; | '|   | :.'|'   '  ;:   |    ; :   |  ;/||   |  \ :  
+:   '   \.   |  ' ' ' :'   :    ;|   |  ||   :     \|   :   .'|   : .  /  
+|   |    '   ;  \; /  ||   |  ./ '   :  ;|   |   . ||   |  |-,;   | |  \  
+'   : |.  \   \  ',  / ;   : ;   |   |  ''   :  '; |'   :  ;/||   | ;\  \ 
+|   | '_\.';   :    /  |   ,/    '   :  ||   |  | ; |   |    \:   ' | \.' 
+'   : |     \   \ .'   '---'     ;   |.' |   :   /  |   :   .':   : :-'   
+;   |,'      `---`               '---'   |   | ,'   |   | ,'  |   |.'     
+'---'                                    `----'     `----'    `---'                                                                       
+"""
+#tworzymy obiekt socketu i probojemy nawiazac sesji tcp z serwerem - socket.SOCK_STREAM TCP
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('192.168.10.1', 55555))
+
+#funkcja do odebrania watku odiborczego, musi dzialac rownolegle bo input() blokuje glowny program
+def handle_messages():
+    global my_id
+    while True:
+        try:
+            #oczekujemy na dane przychodzace z serwera
+            incoming_bytes = client.recv(1024)
+            if not incoming_bytes:
+                break
+                
+            message = incoming_bytes.decode('utf-8')
+            
+            #sprawdzamy czy otrzyywany ciag jest instrukcja sterujaca SET_ID
+            if message.startswith("SET_ID:"):
+                #wydobujemy id
+                my_id = message.split(":")[1]
+            #filtrujmey powiadomienie o dolaczeniu 
+            elif "dolaczyl" in message:
+                print("\r" + CLEAR + YELLOW + BOLD + message + RESET)
+                print(GREEN + "TY (ID-" + my_id + "): " + RESET, end="", flush=True)
+
+            #filtrujemy powiadomienie o odejsciu -78line w serverpy
+            elif "wyszedl" in message:
+                print("\r" + CLEAR + RED + BOLD + message + RESET)
+                print(GREEN + "TY (ID-" + my_id + "): " + RESET, end="", flush=True)
+            
+            else:
+                print("\r" + CLEAR + message)
+                print(GREEN + "TY (ID-" + my_id + "): " + RESET, end="", flush=True)
+                
+        except Exception as e:
+            print("ERROR: odebranie wiadomosci" + str(e))
+            break
+
+#start watku, natomaist daemon = True gwarantuje ze watek zakonczy sie po zamknieciu okna
+receive_handler = threading.Thread(target=handle_messages)
+receive_handler.daemon = True
+receive_handler.start()
+#program wstrzymuje wykonanie dopoki zmienna my_id nie zostanie zaktualizowana przez watek po tym jak zrobi "handshake" z serwerem.
+while my_id == "???":
+    time.sleep(0.1)
+
+#wywolujemy metode clear_console() do wykasowania terminala i wyswietlamy logo 
+clear_console()
+print(ASCII_LOGO)
 
 while True:
-    text = input("Wyslij wiadomosc test:")
-    user.send(text.encode('utf-8'))
+    try:
+        #znak zachety
+        current_prompt = GREEN + "TY (ID-" + my_id + "): " + RESET
+        user_input = input(current_prompt)
+        #jak nacisniemy enter to terminal zostawia wpisany tekst potem uzwamy up i clear aby go wymazac i zastapic lepszym formatowaniem.
+        if user_input:
+            print(UP + "\r" + CLEAR + GREEN + "TY (ID-" + my_id + "): " + user_input + RESET)
+            #wysylamy tekst zmieniony na strumein bajtow 
+            client.send(user_input.encode('utf-8'))
+            
+    except Exception as e:
+        #obsluga bledu jakby np zerwalo polaczenie z brama
+        print("ERROR: blad wyslania " + str(e))
+        break
+
+client.close()
--- src/server.py
+++ src/server.py
@@ -1,32 +1,100 @@
 import socket
 import threading
+import random
+import string
 
-def multiple_users(user, addr):
+#na tej liscie przechowujemy obiekty gniazd socketow dla wszystkich aktywnych userow. Dzieki temu serwer wie do kogo rozsylac wiadomosci itd. 
+user_sockets = []
+
+#na tej liscie natomiast przechowujemy zajete IDki, zebysmy takiego samego nie dali dwom userom, mimo ze jest mala szansa na to.
+assigned_id = []
+
+#funckja generuje losowy 6 znakowy ciag - duze litery plus cyfry. Uzywamy moduul string.
+def generate_id():
+    pool = string.ascii_uppercase + string.digits
+    id_result = ""
+    #wiadomo stringa budujemy petla/ modul random 
+    for i in range(6):
+        char = random.choice(pool)
+        id_result = id_result + char
+    return id_result
+
+#funkcja realizuje logike mostu/relay. Serwer iteruje po liscie aktuwnych socketow i przesyla dane. WAZNE - pominiecie sender_socket, czemu? zeby nadawaca nie otrzymal echa wlasnej wiadomosci
+def handle_broadcast(message_bytes, sender_socket):
+    for user_socket in user_sockets:
+        if user_socket != sender_socket:
+            try:
+                #probojemy przeslac dane przez socketaw formie bajtow
+                user_socket.send(message_bytes)
+            except Exception as e:
+                #jak sie nie uda to wywalamy socket w przypadku np zerwania polaczenia.
+                print("ERROR: rozsylanie wiadomosci" + str(e))
+                if user_socket in user_sockets:
+                    user_sockets.remove(user_socket)
+
+#funkcja do obslugi watku klienta, kazde polaczenie ma pracowac asynchronicznie, aby recv() nie lbokowalo serwra
+def handle_client(user, client_address):
+    #przydzielanie idka
+    session_id = generate_id()
+    #sprawdzamy czy wylosowane id nie jest zajete juz - bylo mowa na zajeciach zeby dodac do funkcji walidacje!!!!
+    while session_id in assigned_id:
+        session_id = generate_id()
+    
+    assigned_id.append(session_id)
+    
+    #Wysylamy informacje o przydzielonym id do usera!!!
+    user.send(("SET_ID:" + session_id).encode('utf-8'))
+    
+    #info systemowe o nowym gosciu - po steonie usera
+    handle_broadcast(("Uzytkownik o ID " + session_id + " dolaczyl.").encode('utf-8'), user)
+    #info z poziomu serwera tez + z jakiego adresu sie polaczyl
+    print("[UBUNTU]: Adres: " + str(client_address) + " ID: " + session_id + " dolaczyl. ")
+
+    #petla nieskonczona while true zeby nasluchiwalo dane przychodzace z konkretnego socketa!!!!
     while True:
         try:
+            #1024 rozmiar bufora
             data = user.recv(1024)
+            #jesli klient wysle pusty pakiet oznacza to zadanie zamkniecia polaczenia juz. czyli jak np zamknie okno terminala
             if not data:
-                print(f"User has disconnected")
                 break
-            message = data.decode('utf-8')
-            print(f"User message: {message}")
+            
+            #tutaj formulowanie juz koncowego pakietu doklejamy id nadawcy do tresci waidomosci
+            text = data.decode('utf-8')
+            full_message = "Anon#" + session_id + ": " + text
+            handle_broadcast(full_message.encode('utf-8'), user)
+            
         except Exception as e:
-            print(f"ERROR: {e}")
+            print("ERROR: uzytkownik " + session_id + " " + str(e))
             break
+
+    print("[UBUNTU]: ID: " + session_id + " sie rozlaczyl. ")
+    
+    #zwalniamy zasoby, kasujemy dziada i usuwamy id jego z listy zajetych i jego gniazdo z aktywnych socketow
+    if session_id in assigned_id:
+        assigned_id.remove(session_id)
+    if user in user_sockets:
+        user_sockets.remove(user)
+    
+    #po stronie usera powiadomienie ze ten i ten o tym id wyszedl z sesji
+    handle_broadcast(("Uzytkownik o ID " + session_id + " wyszedl.").encode('utf-8'), user)
     user.close()
+
 #sockets/gniazda to nic innego jak punkty koncowe komunikacji
 #internet socket i TCP czyli typ gniazda (UDP czy TCP), tworzymy obiekt
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#SO_REUSEADDR pozwala na natychmiastowe bindowanie portu po restarcie zapobiega błedowi TIME_WAIT
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 #rezewrwacja adresu (ubuntu server) i portu w tym przypadku 55555
-server.bind(('192.168.10.1',55555))
+server_socket.bind(('192.168.10.1', 55555))
 #Tutaj wazne, maks 5 polaczen na raz
-server.listen(2)
-print("Server is working")
-print("Waiting for user connection")
-#server gniazdo jest tylko do akceptowania polaczen, zeby userzy mogli sobie gadac potrzeba juz osobnego gniazda tylko dla nich!!!
-while True:
-    user, addr = server.accept()
-    print(f"User with address {addr} has connected")
-    thread = threading.Thread(target=multiple_users, args=(user,addr))
-    thread.start()
+server_socket.listen(5)
 
+print("===== SERWER URUCHOMIONY =====")
+#petla do akceptacji poaczen + odsylanie usera do osobnego watku
+while True:
+    #accept() bedzie tworzyc socket (conn) do rozmowy z userem konretnym
+    conn, client_address = server_socket.accept()
+    user_sockets.append(conn)
+    thread = threading.Thread(target=handle_client, args=(conn, client_address))
+    thread.start()
